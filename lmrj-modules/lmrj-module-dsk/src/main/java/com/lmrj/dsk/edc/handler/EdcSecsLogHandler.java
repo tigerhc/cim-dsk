9d157b37929ca58fedd8fe87f2cec9ecae9c8a65
tortozhang@gmail.com <zhangwj@tfinfo.cn>
2020-05-13 03:18:07 +0800

    事件获取定义描述

--- lmrj-modules/lmrj-module-dsk/src/main/java/com/lmrj/dsk/edc/handler/EdcSecsLogHandler.java
+++ lmrj-modules/lmrj-module-dsk/src/main/java/com/lmrj/dsk/edc/handler/EdcSecsLogHandler.java
@@ -1,9 +1,14 @@
 package com.lmrj.dsk.edc.handler;
 
+import com.lmrj.common.mybatis.mvc.wrapper.EntityWrapper;
 import com.lmrj.edc.ams.entity.EdcAmsRecord;
 import com.lmrj.edc.ams.service.IEdcAmsRecordService;
+import com.lmrj.edc.evt.entity.EdcEvtDefine;
 import com.lmrj.edc.evt.entity.EdcEvtRecord;
+import com.lmrj.edc.evt.service.IEdcEvtDefineService;
 import com.lmrj.edc.evt.service.IEdcEvtRecordService;
+import com.lmrj.fab.eqp.entity.FabEquipment;
+import com.lmrj.fab.eqp.service.IFabEquipmentService;
 import com.lmrj.util.mapper.JsonUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
@@ -19,6 +24,10 @@ public class EdcSecsLogHandler {
     IEdcEvtRecordService edcEvtRecordService;
     @Autowired
     IEdcAmsRecordService edcAmsRecordService;
+    @Autowired
+    IEdcEvtDefineService edcEvtDefineService;
+    @Autowired
+    IFabEquipmentService fabEquipmentService;
 
     @RabbitHandler
     @RabbitListener(queues = {"C2S.Q.ALARM.DATA"})
@@ -36,6 +45,15 @@ public class EdcSecsLogHandler {
     public void handleEvent(String msg) {
         System.out.println("接收到的消息" + msg);
         EdcEvtRecord edcEvtRecord = JsonUtil.from(msg, EdcEvtRecord.class);
+        FabEquipment fabEquipment = fabEquipmentService.findEqpByCode(edcEvtRecord.getEqpId());
+        if(fabEquipment != null){
+            String modelId = fabEquipment.getModelId();
+
+            EdcEvtDefine edcEvtDefine = edcEvtDefineService.selectOne(new EntityWrapper<EdcEvtDefine>().eq("event_id", edcEvtRecord.getEventId()).eq("eqp_model_id", modelId));
+            if(edcEvtDefine != null){
+                edcEvtRecord.setEventDesc(edcEvtDefine.getEventName());
+            }
+        }
         edcEvtRecordService.insert(edcEvtRecord);
     }
 
