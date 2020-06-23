0913d0b758e46b0245c178d03bf83b55c649ae7e
忧伤灬看不透 <hsiguo@163.com>
2020-06-23 14:57:43 +0800

    添加功能信息导出

--- lmrj-modules/lmrj-module-core/src/main/java/com/lmrj/core/sys/controller/SysFunctionController.java
+++ lmrj-modules/lmrj-module-core/src/main/java/com/lmrj/core/sys/controller/SysFunctionController.java
@@ -1,13 +1,19 @@
 package com.lmrj.core.sys.controller;
 
+import com.lmrj.common.http.Response;
 import com.lmrj.common.mvc.annotation.ViewPrefix;
 import com.lmrj.common.mybatis.mvc.controller.BaseCRUDController;
+import com.lmrj.common.query.data.PropertyPreFilterable;
+import com.lmrj.common.query.data.Queryable;
 import com.lmrj.common.security.shiro.authz.annotation.RequiresPathPermission;
 import com.lmrj.core.log.LogAspectj;
 import com.lmrj.core.sys.entity.SysFunction;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 
 /**
@@ -29,4 +35,11 @@ import org.springframework.web.bind.annotation.RestController;
 @LogAspectj(title = "sys_function")
 public class SysFunctionController extends BaseCRUDController<SysFunction> {
 
+    @Override
+    @GetMapping("export")
+    //@LogAspectj(logType = LogType.EXPORT)
+//    @RequiresMethodPermissions("export")
+    public Response export(Queryable queryable, PropertyPreFilterable propertyPreFilterable, HttpServletRequest request, HttpServletResponse response) {
+        return doExport("功能信息", queryable,  propertyPreFilterable,  request,  response);
+    }
 }
--- lmrj-modules/lmrj-module-core/src/main/java/com/lmrj/core/sys/entity/SysFunction.java
+++ lmrj-modules/lmrj-module-core/src/main/java/com/lmrj/core/sys/entity/SysFunction.java
@@ -1,5 +1,6 @@
 package com.lmrj.core.sys.entity;
 
+import cn.afterturn.easypoi.excel.annotation.Excel;
 import com.baomidou.mybatisplus.annotations.TableField;
 import com.baomidou.mybatisplus.annotations.TableName;
 import com.lmrj.core.entity.BaseDataEntity;
@@ -24,18 +25,22 @@ public class SysFunction extends BaseDataEntity {
 
     /**功能类型*/
     @TableField(value = "fct_type")
+    @Excel(name = "功能类型", orderNum = "1", width = 20)
     private String fctType;
     /**功能代码*/
     @TableField(value = "fct_code")
+    @Excel(name = "功能代码", orderNum = "1", width = 40)
     private String fctCode;
     /**功能描述*/
     @TableField(value = "fct_desc")
+    @Excel(name = "功能描述", orderNum = "1", width = 40)
     private String fctDesc;
     /**默认值*/
     @TableField(value = "dafault_value")
     private String dafaultValue;
     /**有效标志*/
     @TableField(value = "active_flag")
+    @Excel(name = "有效标识", orderNum = "1", width = 20)
     private String activeFlag;
 	
 }
