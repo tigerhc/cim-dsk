fc1f1984b5a73d5b67ce55f19352685ca7f3cbb7
忧伤灬看不透 <hsiguo@163.com>
2020-07-21 16:37:03 +0800

    审批配置

--- lmrj-modules/lmrj-module-rms/src/main/java/com/lmrj/rms/permit/mapper/RmsRecipePermitConfigMapper.java
+++ lmrj-modules/lmrj-module-rms/src/main/java/com/lmrj/rms/permit/mapper/RmsRecipePermitConfigMapper.java
@@ -3,8 +3,9 @@ package com.lmrj.rms.permit.mapper;
 import com.lmrj.rms.permit.entity.RmsRecipePermitConfig;
 import com.baomidou.mybatisplus.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Update;
 
- /**
+/**
  * All rights Reserved, Designed By www.lmrj.com
  *
  * @version V1.0
@@ -17,5 +18,8 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface RmsRecipePermitConfigMapper extends BaseMapper<RmsRecipePermitConfig> {
-    
+
+ @Update("update rms_recipe_permit_config set submitter_role_name = #{submitterRoleName} where submit_level = #{submitLevel}")
+ boolean updateRoleNameBySubmitLevel(String submitterRoleName, String submitLevel);
+
 }
--- lmrj-modules/lmrj-module-rms/src/main/java/com/lmrj/rms/permit/service/impl/RmsRecipePermitConfigServiceImpl.java
+++ lmrj-modules/lmrj-module-rms/src/main/java/com/lmrj/rms/permit/service/impl/RmsRecipePermitConfigServiceImpl.java
@@ -1,11 +1,20 @@
 package com.lmrj.rms.permit.service.impl;
 
+import com.lmrj.cim.utils.UserUtil;
 import com.lmrj.common.mybatis.mvc.service.impl.CommonServiceImpl;
+import com.lmrj.common.mybatis.mvc.wrapper.EntityWrapper;
+import com.lmrj.core.sys.entity.User;
 import com.lmrj.rms.permit.service.IRmsRecipePermitConfigService;
 import com.lmrj.rms.permit.entity.RmsRecipePermitConfig;
 import com.lmrj.rms.permit.mapper.RmsRecipePermitConfigMapper;
+import com.lmrj.rms.permit.utils.ShiroExt;
+import org.apache.shiro.SecurityUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+import java.util.List;
 
 
 /**
@@ -23,4 +32,21 @@ import org.springframework.transaction.annotation.Transactional;
 @Service("rmsRecipePermitConfigService")
 public class RmsRecipePermitConfigServiceImpl  extends CommonServiceImpl<RmsRecipePermitConfigMapper,RmsRecipePermitConfig> implements  IRmsRecipePermitConfigService {
 
+    @Autowired
+    private IRmsRecipePermitConfigService rmsRecipePermitConfigService;
+
+    @Override
+    public Integer updateRoleNameBySubmitLevel(String submitterRoleName, String submitLevel) {
+        List<RmsRecipePermitConfig> recipePermitConfigList = rmsRecipePermitConfigService.selectList(new EntityWrapper<RmsRecipePermitConfig>().eq("submit_level", Integer.parseInt(submitLevel)));
+        RmsRecipePermitConfig recipePermitConfig = null;
+        if (recipePermitConfigList.size() > 0){
+            recipePermitConfig = recipePermitConfigList.get(0);
+        }
+        recipePermitConfig.setSubmitterRoleName(submitterRoleName);
+        Object principal = SecurityUtils.getSubject().getPrincipal();
+        String id = ShiroExt.getPrincipalProperty(principal, "id");
+        recipePermitConfig.setUpdateByName(id);
+        recipePermitConfig.setCreateDate(new Date());
+        return baseMapper.updateAllColumnById(recipePermitConfig);
+    }
 }
