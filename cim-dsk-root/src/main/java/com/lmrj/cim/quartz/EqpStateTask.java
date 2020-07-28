5a69d3b16c4c922fa18b32e1c43e58c55023a6e8
zhangwj <zhangwj@lmrj.com>
2020-07-28 17:41:48 +0800

    注释重复类

--- cim-dsk-root/src/main/java/com/lmrj/cim/quartz/EqpStateTask.java
+++ cim-dsk-root/src/main/java/com/lmrj/cim/quartz/EqpStateTask.java
@@ -6,8 +6,8 @@ import org.springframework.stereotype.Component;
 
 import java.util.Calendar;
 
-@Slf4j
-@Component
+//@Slf4j
+//@Component
 public class EqpStateTask {
 
     //@Autowired
@@ -18,17 +18,17 @@ public class EqpStateTask {
      * 每隔10分钟一次
      *
      */
-    @Scheduled(cron = "0 0/10 * * * ?")
-    public void eqpStateDay() {
-        log.info("EqpStateTask定时任务开始执行");
-        //当天时间
-        Calendar cal = Calendar.getInstance();
-        cal.set(Calendar.HOUR_OF_DAY, 0);
-        cal.set(Calendar.MINUTE, 0);
-        cal.set(Calendar.SECOND, 0);
-        //cal.add(Calendar.DAY_OF_MONTH, -1);
-
-        log.info("EqpStateTask定时任务结束执行");
-    }
+//    @Scheduled(cron = "0 0/10 * * * ?")
+//    public void eqpStateDay() {
+//        log.info("EqpStateTask定时任务开始执行");
+//        //当天时间
+//        Calendar cal = Calendar.getInstance();
+//        cal.set(Calendar.HOUR_OF_DAY, 0);
+//        cal.set(Calendar.MINUTE, 0);
+//        cal.set(Calendar.SECOND, 0);
+//        //cal.add(Calendar.DAY_OF_MONTH, -1);
+//
+//        log.info("EqpStateTask定时任务结束执行");
+//    }
 
 }
