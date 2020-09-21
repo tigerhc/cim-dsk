7d2264bcafeef7923a5c46fa892a185f878238a1
zhangwj <zhangwj@tfinfo.cn>
2020-09-21 10:03:29 +0800

    备份产量数据,按天备份

--- cim-dsk-root/src/main/java/com/lmrj/cim/quartz/ProductionBackUpTask.java
+++ cim-dsk-root/src/main/java/com/lmrj/cim/quartz/ProductionBackUpTask.java
@@ -4,6 +4,7 @@ import com.lmrj.dsk.eqplog.entity.EdcDskLogProductionHis;
 import com.lmrj.dsk.eqplog.service.IEdcDskLogProductionHisService;
 import com.lmrj.dsk.eqplog.service.IEdcDskLogProductionService;
 import com.lmrj.fab.eqp.service.IFabEquipmentService;
+import com.lmrj.util.calendar.DateUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -28,34 +29,39 @@ public class ProductionBackUpTask {
      * 备份数据 edc_dsk_log_production -- >   edc_dsk_log_production_his
      */
     //@Scheduled(cron = "0 10 * * * ?")
-    //@Scheduled(cron = "0 0 1 * * ?")
+    //@Scheduled(cron = "0 50 9 * * ?")
     public void backupPdt() {
         log.info("ProductionBackUpTask定时任务开始执行");
         //7天前
         Calendar calstart= Calendar.getInstance();
-        //calstart.add(Calendar.DAY_OF_MONTH, -9);
-        calstart.add(Calendar.DAY_OF_MONTH, -40);
+        calstart.add(Calendar.DAY_OF_MONTH, -4);
         Calendar calEnd = Calendar.getInstance();
         calEnd.add(Calendar.DAY_OF_MONTH, -3);
         List<String> eqpIdlist = fabEquipmentService.findEqpIdList();
-        for (String eqpId : eqpIdlist) {
-            log.info("开始备份"+ eqpId + "设备的数据");
-            while (true){
-                try{
-                    List<EdcDskLogProductionHis> backUpYield = edcDskLogProductionService.findBackUpYield(eqpId,calstart.getTime(), calEnd.getTime());
-                    log.info("ProductionBackUpTask 备份行数: {}", backUpYield.size());
-                    if (backUpYield.size()==0){
-                        break;
+        for(int i=1;i<30; i++){
+            calstart.add(Calendar.DAY_OF_MONTH, -3-i);
+            calEnd.add(Calendar.DAY_OF_MONTH, -2-i);
+            for (String eqpId : eqpIdlist) {
+                int eqpCount = 0;
+                log.info("开始备份"+ eqpId + "设备的数据");
+                while (true){
+                    try{
+                        List<EdcDskLogProductionHis> backUpYield = edcDskLogProductionService.findBackUpYield(eqpId,calstart.getTime(), calEnd.getTime());
+                        log.info("ProductionBackUpTask 备份行数: {}", backUpYield.size());
+                        eqpCount = eqpCount+ backUpYield.size();
+                        if (backUpYield.size()==0){
+                            break;
+                        }
+                        edcDskLogProductionHisService.insertBatch(backUpYield,2);
+                    }catch (Exception e){
+                        log.error("Exception:",e);
                     }
-                    edcDskLogProductionHisService.insertBatch(backUpYield,2);
-                }catch (Exception e){
-                    log.error("Exception:",e);
                 }
-
+                log.info( "{}产量数据备份完成,{}--{}备份:{}",eqpId, DateUtil.formatTime(calstart.getTime()),DateUtil.formatTime(calEnd.getTime()) ,  eqpCount);
             }
-            log.info(eqpId + "设备的数据备份完成");
         }
-        // TODO: 2020/6/8
+
         log.info("ProductionBackUpTask定时任务开始执行结束");
     }
+
 }
