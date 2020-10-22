27d0859e527a54296ce98dbefc7dda2b9637ec38
GaoXuejun <1518798637@qq.com>
2020-10-22 08:32:22 +0800

    修改WB trackout逻辑

--- lmrj-modules/lmrj-module-mes/src/main/java/com/lmrj/mes/track/service/impl/MesLotTrackServiceImpl.java
+++ lmrj-modules/lmrj-module-mes/src/main/java/com/lmrj/mes/track/service/impl/MesLotTrackServiceImpl.java
@@ -384,7 +384,7 @@ public class MesLotTrackServiceImpl extends CommonServiceImpl<MesLotTrackMapper,
             return MesResult.error("eqp not found");
         }
         for (FabEquipment fabEquipment : fabEquipmentList) {
-            result = doTrackout(fabEquipment, productionNo, productionName, orderNo, lotNo, "4896", recipeCode, opId);
+            result = doTrackout(fabEquipment, productionNo, productionName, orderNo, lotNo, "2448", recipeCode, opId);
             if (!"Y".equals(result.getFlag())) {
                 return result; //失败提前退出
             }
