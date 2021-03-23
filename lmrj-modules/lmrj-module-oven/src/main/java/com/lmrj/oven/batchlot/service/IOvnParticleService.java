aaeefa0b6121916f92604130440fd966f9ad8879
dongle <donglele_27@126.com>
2021-03-23 15:14:14 +0800

    尘埃粒子计数器

---
+++ lmrj-modules/lmrj-module-oven/src/main/java/com/lmrj/oven/batchlot/service/IOvnParticleService.java
@@ -0,0 +1,7 @@
+package com.lmrj.oven.batchlot.service;
+
+import com.lmrj.common.mybatis.mvc.service.ICommonService;
+import com.lmrj.oven.batchlot.entity.ParticleDataBean;
+
+public interface IOvnParticleService extends ICommonService<ParticleDataBean> {
+}
