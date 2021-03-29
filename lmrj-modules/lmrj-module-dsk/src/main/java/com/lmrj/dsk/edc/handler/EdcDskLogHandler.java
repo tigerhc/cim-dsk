9a5a86194d8c54489550611f6d191d1f2f8f8252
GaoXuejun <1518798637@qq.com>
2021-03-29 10:07:05 +0800

    修改邮件发送队列处理逻辑

--- lmrj-modules/lmrj-module-dsk/src/main/java/com/lmrj/dsk/edc/handler/EdcDskLogHandler.java
+++ lmrj-modules/lmrj-module-dsk/src/main/java/com/lmrj/dsk/edc/handler/EdcDskLogHandler.java
@@ -678,11 +678,13 @@ public class EdcDskLogHandler {
         List<Map<String, Object>> department = fabEquipmentService.findDepartment(eqpId);
         if(!alarmCode.equals(":网络断开连接!")){
             users = fabEquipmentService.findEmailALL(alarmCode);
-            if("E-0009".equals(alarmCode)){
+            /*if("E-0009".equals(alarmCode)){
                 code = alarmCode;
             }else if("E-0010".equals(alarmCode)){
                 code = alarmCode;
-            }
+            }else if("E-0011".equals(alarmCode)){
+                code = alarmCode;
+            }*/
         }else if (department.get(0).get("department").equals("YK")) {
             users = fabEquipmentService.findEmailALL("E-0007");
         } else if (department.get(0).get("department").equals("EK")) {
