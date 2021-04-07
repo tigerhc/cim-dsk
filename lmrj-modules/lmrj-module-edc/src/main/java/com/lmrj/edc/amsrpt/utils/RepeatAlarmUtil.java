3e3f3997d633fa61a6a6e32b9ea8b53aeb9fac7c
zhangyanan <1940185357@qq.com>
2021-04-07 16:16:49 +0800

    修改警报处理方法让其适用邮件发送

--- lmrj-modules/lmrj-module-edc/src/main/java/com/lmrj/edc/amsrpt/utils/RepeatAlarmUtil.java
+++ lmrj-modules/lmrj-module-edc/src/main/java/com/lmrj/edc/amsrpt/utils/RepeatAlarmUtil.java
@@ -1,11 +1,16 @@
 package com.lmrj.edc.amsrpt.utils;
 
+import com.google.common.collect.Maps;
 import com.lmrj.common.mybatis.mvc.wrapper.EntityWrapper;
 import com.lmrj.core.email.service.IEmailSendService;
+import com.lmrj.edc.ams.entity.EdcAmsDefine;
 import com.lmrj.edc.ams.entity.EdcAmsRecord;
+import com.lmrj.edc.ams.service.IEdcAmsDefineService;
 import com.lmrj.edc.amsrpt.entity.EdcAmsRptDefine;
+import com.lmrj.edc.amsrpt.entity.EdcAmsRptDefineActEmail;
 import com.lmrj.edc.amsrpt.entity.EdcAmsRptRecord;
 import com.lmrj.edc.amsrpt.entity.EdcAmsRptRecordDtl;
+import com.lmrj.edc.amsrpt.service.IEdcAmsRptDefineActEmailService;
 import com.lmrj.edc.amsrpt.service.IEdcAmsRptDefineService;
 import com.lmrj.edc.amsrpt.service.IEdcAmsRptRecordDtlService;
 import com.lmrj.edc.amsrpt.service.IEdcAmsRptRecordService;
@@ -55,17 +60,31 @@ public class RepeatAlarmUtil {
     private IFabEquipmentService fabEquipmentService;
     @Autowired
     private IFabEquipmentModelService fabEquipmentModelService;
+    @Autowired
+    private IEdcAmsDefineService edcAmsDefineService;
+    @Autowired
+    private IEdcAmsRptDefineActEmailService edAmsRptEamilService;
 
     public void queryAlarmDefine(){
         //先查询alarm配置保存在redis中
         if (redisTemplate.opsForList().size("amsRptDefineList") > 0){
-            return;
         }else {
             List<EdcAmsRptDefine> amsRptDefineList = edcAmsRptDefineService.selectList(new EntityWrapper<>());
             for (EdcAmsRptDefine amsRptDefine:amsRptDefineList) {
                 redisTemplate.opsForList().rightPush("amsRptDefineList", amsRptDefine);
             }
         }
+
+
+        if (redisTemplate.opsForList().size("amsDefineList") > 0){
+            return;
+        }else {
+            List<EdcAmsDefine> amsDefineList = edcAmsDefineService.selectList(new EntityWrapper<>());
+            for (EdcAmsDefine amsDefine:amsDefineList) {
+                redisTemplate.opsForList().rightPush("amsDefineList", amsDefine);
+            }
+        }
+
     }
 
     //将报警信息放入消息队列
@@ -84,17 +103,77 @@ public class RepeatAlarmUtil {
         log.info("start 检查报警信息{} : {}", edcAmsRecord.getEqpId(), edcAmsRecord.getAlarmCode());
         //先看是不是配置过的alarm
         List<EdcAmsRptDefine> amsRptDefineList = redisTemplate.opsForList().range("amsRptDefineList", 0, -1);
-        for (EdcAmsRptDefine amsRptDefine:amsRptDefineList) {
-            if (amsRptDefine.getAlarmId().equals(edcAmsRecord.getAlarmCode())){
+        List<EdcAmsDefine> amsDefineList = redisTemplate.opsForList().range("amsDefineList", 0, 1);//edcAmsDefineService.selectList(new EntityWrapper<>());//
+        for (EdcAmsDefine amsDefine:amsDefineList){
+            if(amsDefine.getAlarmCode().equals(edcAmsRecord.getAlarmCode())){
                 //如果alarm_id是否符合
                 FabEquipment fabEquipment = fabEquipmentService.findEqpByCode(edcAmsRecord.getEqpId());
-                if (fabEquipment.getModelId().equals(amsRptDefine.getEqpModelId())){
+               if (fabEquipment.getModelId().equals(amsDefine.getEqpModelId())){
                     //如果设备类型是否符合
-                    resolveRepeatAlarm(amsRptDefine, edcAmsRecord);
-                    return;
+                   for (EdcAmsRptDefine amsRptDefine:amsRptDefineList) {
+                      if(amsRptDefine.getAlarmId().equals(amsDefine.getId())) {
+                          //如果设备类型是否符合
+
+                          //增加警报类型逻辑如果是rpt-alarm 则立刻发送邮件否则逻辑不变
+                          if(!"param_check".equals(edcAmsRecord.getAlarmCode())){
+                              resolveRepeatAlarm(amsRptDefine, edcAmsRecord);
+                          }else{
+                            //邮件发送
+                            //获取邮件发送人
+                            Map<String,Object> map = new HashMap<String,Object>();
+                              map.put("rpt_alarm_id",amsRptDefine.getId());
+                              List<EdcAmsRptDefineActEmail> edcAmsRptEmailList = edAmsRptEamilService.selectByMap(map);
+                              if(edcAmsRptEmailList!=null&&edcAmsRptEmailList.size()>0){
+                                  List<String> emailList = new ArrayList<String>();
+                                  for (EdcAmsRptDefineActEmail edcAmsRptEmail:edcAmsRptEmailList) {
+                                      emailList.add(edcAmsRptEmail.getUserEmail());
+                                  }
+                                  String[] email = emailList.toArray(new String[emailList.size()]);
+                                  Map<String, Object> datas = Maps.newHashMap();
+                                  String paramCOde = "";
+                                  String recipeCode = "";
+                                  String[] detail = edcAmsRecord.getAlarmDetail()==null?null:edcAmsRecord.getAlarmDetail().split(",");
+                                  if(detail!=null&&detail.length>=2){
+                                      recipeCode = detail[0];
+                                      paramCOde = detail[1];
+                                  }
+                                  datas.put("PARAM_CODE", paramCOde);
+                                  datas.put("RECIPE_CODE", recipeCode);
+                                  datas.put("EQP_ID", edcAmsRecord.getEqpId());
+                                  emailSendService.send(email,"PARAM_CHECK",datas);
+                                  EdcAmsRptRecord edcAmsRptRecord = new EdcAmsRptRecord();
+                                  edcAmsRptRecord.setAlarmCode(edcAmsRecord.getAlarmCode());
+                                  edcAmsRptRecord.setAlarmId(edcAmsRecord.getId());
+                                  edcAmsRptRecord.setAlarmName(edcAmsRecord.getAlarmName());
+                                  edcAmsRptRecord.setEqpId(edcAmsRecord.getEqpId());
+                                  edcAmsRptRecord.setEqpModelId(amsRptDefine.getEqpModelId());
+                                  edcAmsRptRecord.setEqpModelName(fabEquipmentModelService.selectList(new EntityWrapper<FabEquipmentModel>().eq("id", amsRptDefine.getEqpModelId())).get(0).getModelName());
+                                  edcAmsRptRecord.setLotNo(edcAmsRecord.getLotNo());
+                                  edcAmsRptRecord.setRptAlarmId(amsRptDefine.getAlarmId());
+                                  //保存一条edcAmsRptRecord记录
+                                  edcAmsRptRecordService.insert(edcAmsRptRecord);
+                              }
+
+                              //清空集合并将数据保存到edc_ams_rpt_record和edc_ams_rpt_record_dtl表中
+                          }
+
+                          return;
+                      }
+                   }
                 }
             }
         }
+//        for (EdcAmsRptDefine amsRptDefine:amsRptDefineList) {
+////            if (amsRptDefine.getAlarmId().equals(edcAmsRecord.getAlarmCode())){
+////                //如果alarm_id是否符合
+////                FabEquipment fabEquipment = fabEquipmentService.findEqpByCode(edcAmsRecord.getEqpId());
+////                if (fabEquipment.getModelId().equals(amsRptDefine.getEqpModelId())){
+////                    //如果设备类型是否符合
+////                    resolveRepeatAlarm(amsRptDefine, edcAmsRecord);
+////                    return;
+////                }
+////            }
+////        }
     }
 
     private void resolveRepeatAlarm(EdcAmsRptDefine amsRptDefine ,EdcAmsRecord edcAmsRecord){
@@ -112,7 +191,7 @@ public class RepeatAlarmUtil {
                 //锁住设备
                 eqptService.lockEqpt("1", edcAmsRecord.getEqpId(),"ALARM_LOCK");
                 //TODO  发送邮件
-//                emailSendService.sendEmail("ALARM","","RepeatAlarm信息",
+//                .sendEmail("ALARM","","RepeatAlarm信息",
 //                        "机台:" + edcAmsRecord.getEqpId() + ",触发RepeatAlarm,报警信息如下：" + "\n"
 //                                + "报警id:" + edcAmsRecord.getAlarmCode() + ",报警描述:"
 //                                + edcAmsRecord.getAlarmName() + "\n" + "请速联系相关操作人员消警！");
