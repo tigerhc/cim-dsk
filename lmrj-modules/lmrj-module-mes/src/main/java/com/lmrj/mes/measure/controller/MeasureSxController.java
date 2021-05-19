610e52ae495b8ac6fedc4d4af09e366e3b56c85b
wangdong <donglele_27@126.com>
2021-05-19 14:23:58 +0800

    分离的导出加时间和上下限

--- lmrj-modules/lmrj-module-mes/src/main/java/com/lmrj/mes/measure/controller/MeasureSxController.java
+++ lmrj-modules/lmrj-module-mes/src/main/java/com/lmrj/mes/measure/controller/MeasureSxController.java
@@ -76,55 +76,88 @@ public class MeasureSxController {
             keyList.add(key3);
             ExcelExportEntity key4 = new ExcelExportEntity("串行计数器","4");
             keyList.add(key4);
-            ExcelExportEntity key5 = new ExcelExportEntity("1:A","5");
+            ExcelExportEntity key5 = new ExcelExportEntity("时间","5");
             keyList.add(key5);
-            ExcelExportEntity key6 = new ExcelExportEntity("1:B","6");
+            ExcelExportEntity key6 = new ExcelExportEntity("1:A","6");
             keyList.add(key6);
-            ExcelExportEntity key7 = new ExcelExportEntity("1:C","7");
+            ExcelExportEntity key7 = new ExcelExportEntity("1:B","7");
             keyList.add(key7);
-            ExcelExportEntity key8 = new ExcelExportEntity("1:D","8");
+            ExcelExportEntity key8 = new ExcelExportEntity("1:C","8");
             keyList.add(key8);
-            ExcelExportEntity key9 = new ExcelExportEntity("2:A","9");
+            ExcelExportEntity key9 = new ExcelExportEntity("1:D","9");
             keyList.add(key9);
-            ExcelExportEntity key10 = new ExcelExportEntity("2:B","10");
+            ExcelExportEntity key10 = new ExcelExportEntity("2:A","10");
             keyList.add(key10);
-            ExcelExportEntity key11 = new ExcelExportEntity("2:C","11");
+            ExcelExportEntity key11 = new ExcelExportEntity("2:B","11");
             keyList.add(key11);
-            ExcelExportEntity key12 = new ExcelExportEntity("2:D","12");
+            ExcelExportEntity key12 = new ExcelExportEntity("2:C","12");
             keyList.add(key12);
-
+            ExcelExportEntity key13 = new ExcelExportEntity("2:D","13");
+            keyList.add(key13);
             for (int i = 0; i <size ; i++) {
                 Map<String, String> data = new HashMap<>() ;
                 data.put("1",type);
                 data.put("2",productionName);
                 data.put("3",result.get(i).get("lotNo"));
                 data.put("4","1");
-                data.put("5",result.get(i).get("a1"));
-                data.put("6",result.get(i).get("b1"));
-                data.put("7",result.get(i).get("c1"));
-                data.put("8",result.get(i).get("d1"));
-                data.put("9",result.get(i).get("a2"));
-                data.put("10",result.get(i).get("b2"));
-                data.put("11",result.get(i).get("c2"));
-                data.put("12",result.get(i).get("d2"));
+                data.put("5",result.get(i).get("meaDate"));
+                data.put("6",result.get(i).get("a1"));
+                data.put("7",result.get(i).get("b1"));
+                data.put("8",result.get(i).get("c1"));
+                data.put("9",result.get(i).get("d1"));
+                data.put("10",result.get(i).get("a2"));
+                data.put("11",result.get(i).get("b2"));
+                data.put("12",result.get(i).get("c2"));
+                data.put("13",result.get(i).get("d2"));
                 dataList.add(data);
                 data = new HashMap<>() ;
                 data.put("1",type);
                 data.put("2",productionName);
                 data.put("3",result.get(i).get("lotNo"));
                 data.put("4","2");
-                data.put("5",result2.get(i).get("a1"));
-                data.put("6",result2.get(i).get("b1"));
-                data.put("7",result2.get(i).get("c1"));
-                data.put("8",result2.get(i).get("d1"));
-                data.put("9",result2.get(i).get("a2"));
-                data.put("10",result2.get(i).get("b2"));
-                data.put("11",result2.get(i).get("c2"));
-                data.put("12",result2.get(i).get("d2"));
+                data.put("5",result2.get(i).get("meaDate"));
+                data.put("6",result2.get(i).get("a1"));
+                data.put("7",result2.get(i).get("b1"));
+                data.put("8",result2.get(i).get("c1"));
+                data.put("9",result2.get(i).get("d1"));
+                data.put("10",result2.get(i).get("a2"));
+                data.put("11",result2.get(i).get("b2"));
+                data.put("12",result2.get(i).get("c2"));
+                data.put("13",result2.get(i).get("d2"));
+                dataList.add(data);
+                data = new HashMap<>() ;//%3行 下限
+                data.put("1",type);
+                data.put("2",productionName);
+                data.put("3",result.get(i).get("lotNo"));
+                data.put("4","下限");
+                data.put("5","");//时间
+                data.put("6","13.9");
+                data.put("7","0.4");
+                data.put("8","0.07");
+                data.put("9","0");
+                data.put("10","13.9");
+                data.put("11","0.4");
+                data.put("12","0.07");
+                data.put("13","0");
+                dataList.add(data);
+                data = new HashMap<>() ;//%4行 上限
+                data.put("1",type);
+                data.put("2",productionName);
+                data.put("3",result.get(i).get("lotNo"));
+                data.put("4","上限");
+                data.put("5","");//时间
+                data.put("6","14.3");
+                data.put("7","1");
+                data.put("8","0.13");
+                data.put("9","17");
+                data.put("10","14.3");
+                data.put("11","1");
+                data.put("12","0.13");
+                data.put("13","17");
                 dataList.add(data);
             }
 
-            Workbook book = ExcelExportUtil.exportExcel(new ExportParams("量测分离","量测详细信息"),keyList,dataList);
+            Workbook book = ExcelExportUtil.exportExcel(new ExportParams("量测分离"+productionName,"量测详细信息"),keyList,dataList);
             FileOutputStream fos = new FileOutputStream("D:/ExcelExportForMap.xls");
             book.write(fos);
             fos.close();
@@ -140,7 +173,5 @@ public class MeasureSxController {
             var16.printStackTrace();
             return Response.error(999998, "导出失败");
         }
-
-
     }
 }
