863366a91997bdfc0d6f9f7a4973c5bfdabe1d2f
GaoXuejun <1518798637@qq.com>
2021-05-27 14:47:50 +0800

    修改判断字段

--- lmrj-modules/lmrj-module-mes/src/main/java/com/lmrj/mes/track/controller/MesLotTrackController.java
+++ lmrj-modules/lmrj-module-mes/src/main/java/com/lmrj/mes/track/controller/MesLotTrackController.java
@@ -499,9 +499,9 @@ public class MesLotTrackController extends BaseCRUDController<MesLotTrack> {
                 eqpId1 = "APJ-IGBT-SORT1";
             } else if (eqpId1.contains("FED")) {
                 eqpId1 = "APJ-FRD-SORT1";
-            } else if (eqpId1.contains("IGBTUJH")) {
+            } else if (eqpId1.contains("IGBTYJH")) {
                 eqpId1 = "APJ-IGBT-SORT3";
-            } else if (eqpId1.contains("FRDUJH")) {
+            } else if (eqpId1.contains("FRDYJH")) {
                 eqpId1 = "APJ-FRD-SORT3";
             } else if (eqpId1.contains("RY1")) {
                 eqpId1 = "APJ-HB1-SORT2";
