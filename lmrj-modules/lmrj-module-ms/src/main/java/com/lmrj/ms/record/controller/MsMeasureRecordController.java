55f1f1bcd6b6c67c657ebd9e8ce0395cc26b7a71
张伟江 <472109366@qq.com>
2021-06-19 10:20:03 +0800

    查询第二条记录

--- lmrj-modules/lmrj-module-ms/src/main/java/com/lmrj/ms/record/controller/MsMeasureRecordController.java
+++ lmrj-modules/lmrj-module-ms/src/main/java/com/lmrj/ms/record/controller/MsMeasureRecordController.java
@@ -139,6 +139,14 @@ public class MsMeasureRecordController extends BaseCRUDController<MsMeasureRecor
         ServletUtils.printJson(response, content);
     }
 
+    @RequestMapping(value = "/msrecordbyfirst/{eqpId}", method = { RequestMethod.GET, RequestMethod.POST })
+    public void msrecordbyfirst(@PathVariable String eqpId, @RequestParam String processNo ,@RequestParam String productionNo, @RequestParam String lotNo ,@RequestParam String waferId,
+                                  HttpServletRequest request, HttpServletResponse response){
+        MsMeasureRecord record =  msMeasureRecordService.findMsrecordbyfirst(eqpId, processNo, productionNo, lotNo,waferId);
+        String content = JSON.toJSONStringWithDateFormat(record, JSON.DEFFAULT_DATE_FORMAT);
+        ServletUtils.printJson(response, content);
+    }
+
     @Override
     @GetMapping("export")
     //@LogAspectj(logType = LogType.EXPORT)
