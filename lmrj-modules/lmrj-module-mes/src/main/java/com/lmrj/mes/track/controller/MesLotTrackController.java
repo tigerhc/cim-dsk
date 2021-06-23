56eb5bd0448eb691228cfbe8f98ebe52317fc508
GaoXuejun <1518798637@qq.com>
2021-06-23 10:47:26 +0800

    增加TRM参数获取接口

--- lmrj-modules/lmrj-module-mes/src/main/java/com/lmrj/mes/track/controller/MesLotTrackController.java
+++ lmrj-modules/lmrj-module-mes/src/main/java/com/lmrj/mes/track/controller/MesLotTrackController.java
@@ -620,6 +620,39 @@ public class MesLotTrackController extends BaseCRUDController<MesLotTrack> {
         }
     }
 
+    //查找APJ-TRM设备参数，从产量日志和配方日志中获取
+    @RequestMapping(value = "/findTrmParam/{eqpId}", method = {RequestMethod.GET, RequestMethod.POST})
+    public String findTrmParam(Model model, @PathVariable String eqpId, @RequestParam String opId,
+                               HttpServletRequest request, HttpServletResponse response) {
+        log.info("findTrmParam :  {}, {}", opId, eqpId);
+        String eventDesc = "{\"eqpId\":\"" + eqpId + "\",\"opId\":\"" + opId + "\"}";//日志记录参数
+        try {
+            fabLogService.info(eqpId, "Param16", "MesLotTrackController.findTrmParam", eventDesc, "", "wangdong");//日志记录参数
+            //String eqpId ="SIM-DM1";
+            if ("".equals(opId) || opId == null) {
+                return "opId Cannot be empty";
+            }
+            if (eqpId.contains("TRM")) {
+
+            } else {
+                log.error("设备名称错误！   " + eqpId);
+                return "eqpId error!:" + eqpId;
+            }
+            String methodName = "FIND_TRM_PARAM";
+            MesResult result = mesLotTrackService.findApjParam(eqpId, methodName, opId);
+            JSONObject jo = JSONObject.fromObject(result);//日志记录结果
+            fabLogService.info(eqpId, "Result16", "MesLotTrackController.findViParam", jo.toString(), eqpId, "wangdong");//日志记录
+            if ("Y".equals(result.getFlag())) {
+                return result.getContent().toString();
+            } else {
+                return result.getMsg();
+            }
+        } catch (Exception e) {
+            fabLogService.info(eqpId, "Error16", "MesLotTrackController.findViParam", "有异常", eqpId, "wangdong");//日志记录
+            return e.getMessage();
+        }
+    }
+
 
     //36916087020DM____0507A5002915J.SIM6812M(E)D-URA_F2971_
     @RequestMapping(value = "/dsktrackin2/{eqpId}", method = {RequestMethod.GET, RequestMethod.POST})
